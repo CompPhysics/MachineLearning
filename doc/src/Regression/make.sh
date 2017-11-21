d52541b08e70ee6e22490b2a55508e8b63d08812
mhjensen <morten.hjorth-jensen@fys.uio.no>
2017-11-21 17:46:17 +0100

    Testing doconce with python3.6

--- doc/src/Regression/make.sh
+++ doc/src/Regression/make.sh
@@ -44,10 +44,10 @@ system doconce split_html $html.html --method=space10
 # Bootstrap style
 html=${name}-bs
 system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
-system doconce split_html $html.html --method=split --pagination --nav_button=bottom
+#system doconce split_html $html.html --method=split --pagination --nav_button=bottom
 
 # IPython notebook
-#system doconce format ipynb $name $opt
+system doconce format ipynb $name $opt
 
 # LaTeX Beamer slides
 beamertheme=red_plain
