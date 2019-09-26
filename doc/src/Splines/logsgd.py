24378a552b68c8df393bf3309ee30578f4e4f125
mhjensen <morten.hjorth-jensen@fys.uio.no>
2019-09-26 06:25:38 +0200

    changed deadline fr project 1

--- doc/src/Splines/logsgd.py
+++ doc/src/Splines/logsgd.py
@@ -5,12 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDRegressor
 
 x = 2*np.random.rand(100,1)
-y = 4+3*x+np.random.randn(100,1)
+y = 4+3*x#+np.random.randn(100,1)
 
 xb = np.c_[np.ones((100,1)), x]
-theta_linreg = np.linalg.inv(xb.T.dot(xb)).dot(xb.T).dot(y)
-print("Own inversion")
-print(theta_linreg)
+
 sgdreg = SGDRegressor(max_iter = 50, penalty=None, eta0=0.1)
 sgdreg.fit(x,y.ravel())
 print("sgdreg from scikit")
@@ -18,13 +16,12 @@ print(sgdreg.intercept_, sgdreg.coef_)
 
 
 theta = np.random.randn(2,1)
-
 eta = 0.1
 Niterations = 1000
 m = 100
 
 for iter in range(Niterations):
-    gradients = 2.0/m*xb.T.dot(xb.dot(theta)-y)
+    gradients = 2.0/m*xb.T @ ((xb @ theta)-y)
     theta -= eta*gradients
 print("theta from own gd")
 print(theta)
@@ -32,7 +29,6 @@ print(theta)
 xnew = np.array([[0],[2]])
 xbnew = np.c_[np.ones((2,1)), xnew]
 ypredict = xbnew.dot(theta)
-ypredict2 = xbnew.dot(theta_linreg)
 
 n_epochs = 50
 t0, t1 = 5, 50
@@ -47,14 +43,13 @@ for epoch in range(n_epochs):
         random_index = np.random.randint(m)
         xi = xb[random_index:random_index+1]
         yi = y[random_index:random_index+1]
-        gradients = 2 * xi.T.dot(xi.dot(theta)-yi)
+        gradients = 2 * xi.T @ ((xi @ theta)-yi)
         eta = learning_schedule(epoch*m+i)
         theta = theta - eta*gradients
 print("theta from own sdg")
 print(theta)
 
 plt.plot(xnew, ypredict, "r-")
-plt.plot(xnew, ypredict2, "b-")
 plt.plot(x, y ,'ro')
 plt.axis([0,2.0,0, 15.0])
 plt.xlabel(r'$x$')
