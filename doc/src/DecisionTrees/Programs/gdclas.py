29ed6fd9be7cbfedbee588ce28c81d118a77d1d6
mhjensen <morten.hjorth-jensen@fys.uio.no>
2019-11-08 06:41:51 +0100

    added examples

--- doc/src/DecisionTrees/Programs/gdclas.py
+++ doc/src/DecisionTrees/Programs/gdclas.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import  train_test_split 
 from sklearn.datasets import load_breast_cancer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import cross_validate
 import scikitplot as skplt
-import xgboost as xgb
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import cross_validate
+
 # Load the data
 cancer = load_breast_cancer()
 
@@ -19,28 +19,19 @@ scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-xg_clf = xgb.XGBClassifier(max_depth = 4, n_estimators = 200)
-xg_clf.fit(X_train_scaled,y_train)
-
-y_test = xg_clf.predict(X_test_scaled)
-
-print("Test set accuracy with Random Forests and scaled data: {:.2f}".format(xg_clf.score(X_test_scaled,y_test)))
+gd_clf = GradientBoostingClassifier(max_depth=3, n_estimators=100, learning_rate=1.0)  
+gd_clf.fit(X_train_scaled, y_train)
+#Cross validation
+accuracy = cross_validate(gd_clf,X_test_scaled,y_test,cv=10)['test_score']
+print(accuracy)
+print("Test set accuracy with Random Forests and scaled data: {:.2f}".format(gd_clf.score(X_test_scaled,y_test)))
 
 import scikitplot as skplt
-y_pred = xg_clf.predict(X_test_scaled)
+y_pred = gd_clf.predict(X_test_scaled)
 skplt.metrics.plot_confusion_matrix(y_test, y_pred, normalize=True)
 plt.show()
-y_probas = xg_clf.predict_proba(X_test_scaled)
+y_probas = gd_clf.predict_proba(X_test_scaled)
 skplt.metrics.plot_roc(y_test, y_probas)
 plt.show()
 skplt.metrics.plot_cumulative_gain(y_test, y_probas)
-plt.show()
-
-
-xgb.plot_tree(xg_clf,num_trees=0)
-plt.rcParams['figure.figsize'] = [50, 10]
-plt.show()
-
-xgb.plot_importance(xg_clf)
-plt.rcParams['figure.figsize'] = [5, 5]
 plt.show()
