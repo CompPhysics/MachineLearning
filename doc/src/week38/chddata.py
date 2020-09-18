281ce1e14a65227f64e171dc5e154e8a1cd02cd5
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2020-09-18 05:34:28 +0200

    update on logreg slides

--- doc/src/week38/chddata.py
+++ doc/src/week38/chddata.py
@@ -8,6 +8,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
 from sklearn.metrics import mean_squared_error
 from IPython.display import display
+from pylab import plt, mpl
+plt.style.use('seaborn')
+mpl.rcParams['font.family'] = 'serif'
+
 # Where to save the figures and data files
 PROJECT_ROOT_DIR = "Results"
 FIGURE_ID = "Results/FigureFiles"
@@ -33,11 +37,27 @@ def save_fig(fig_id):
 
 infile = open(data_path("chddata.csv"),'r')
 
-# Read the chd data as  csv file and organize the data into two arrays with density and energies
+# Read the chd data as  csv file and organize the data into arrays with age group, age, and chd
 chd = pd.read_csv(infile, names=('ID', 'Age', 'Agegroup', 'CHD'))
-display(chd)
+chd.columns = ['ID', 'Age', 'Agegroup', 'CHD']
 output = chd['CHD']
 age = chd['Age']
 agegroup = chd['Agegroup']
 numberID  = chd['ID'] 
-display(output)
+display(chd)
+
+plt.scatter(age, output, marker='o')
+plt.axis([18,70.0,-0.1, 1.2])
+plt.xlabel(r'Age')
+plt.ylabel(r'CHD')
+plt.title(r'Age distribution and Coronary heart disease')
+plt.show()
+
+agegroupmean = np.array([0.1, 0.133, 0.250, 0.333, 0.462, 0.625, 0.765, 0.800])
+group = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+plt.plot(group, agegroupmean, "r-")
+plt.axis([0,9,0, 1.0])
+plt.xlabel(r'Age group')
+plt.ylabel(r'CHD mean values')
+plt.title(r'Mean values for each age group')
+plt.show()
