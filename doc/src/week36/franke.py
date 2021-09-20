ca4dc3db8b45a8ee6e4fb1387ad0292769ba1fa7
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2021-09-20 23:07:01 +0200

    added file

--- doc/src/week36/franke.py
+++ doc/src/week36/franke.py
@@ -4,32 +4,46 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 
-def R2(y_data, y_model):
-    return 1 - np.sum((y_data - y_model) ** 2) / np.sum((y_data - np.mean(y_data)) ** 2)
+
 def MSE(y_data,y_model):
     n = np.size(y_model)
     return np.sum((y_data-y_model)**2)/n
 
-
-# A seed just to ensure that the random numbers are the same for every run.
-# Useful for eventual debugging.
-np.random.seed(3155)
-
-n = 100
-x = np.random.rand(n)
-y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2)
-
-Maxpolydegree = 20
-X = np.zeros((n,Maxpolydegree))
-X[:,0] = 1.0
-
-for polydegree in range(1, Maxpolydegree):
-    for degree in range(polydegree):
-        X[:,degree] = x**degree
+def FrankeFunction(x,y):
+	term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+	term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+	term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+	return term1 + term2 + term3 + term4
 
 
+def create_X(x, y, n ):
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+
+	N = len(x)
+	l = int((n+1)*(n+2)/2)		# Number of elements in beta
+	X = np.ones((N,l))
+
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = (x**(i-k))*(y**k)
+
+	return X
+
+
+# Making meshgrid of datapoints and compute Franke's function
+n = 5
+N = 1000
+x = np.sort(np.random.uniform(0, 1, N))
+y = np.sort(np.random.uniform(0, 1, N))
+z = FrankeFunction(x, y)
+X = create_X(x, y, n=n)    
+    
 # We split the data in test and training data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, z, test_size=0.2)
 
 # matrix inversion to find beta
 OLSbeta = np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ y_train
@@ -45,7 +59,7 @@ print(MSE(y_test,ypredictOLS))
 p = len(OLSbeta)
 I = np.eye(p,p)
 # Decide which values of lambda to use
-nlambdas = 4
+nlambdas = 5
 MSEOwnRidgePredict = np.zeros(nlambdas)
 MSEOwnRidgeTrain = np.zeros(nlambdas)
 MSERidgePredict = np.zeros(nlambdas)
@@ -83,4 +97,10 @@ plt.xlabel('log10(lambda)')
 plt.ylabel('MSE')
 plt.legend()
 plt.show()
+
+
+
+
+
+
 
