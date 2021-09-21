f1476343ed7b330cbe2e80d5a7c9d60297bcfcc5
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2021-09-21 13:08:18 +0200

    changes to week 38

--- doc/src/week36/scale.py
+++ doc/src/week36/scale.py
@@ -22,11 +22,10 @@ y = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2)
 
 Maxpolydegree = 5
 X = np.zeros((n,Maxpolydegree))
-X[:,0] = 1.0
+#X[:,0] = 1.0
 
-for polydegree in range(1, Maxpolydegree):
-    for degree in range(polydegree):
-        X[:,degree] = x**(degree)
+for degree in range(Maxpolydegree):
+    X[:,degree] = x**(degree)
 
 
 
@@ -35,15 +34,15 @@ for polydegree in range(1, Maxpolydegree):
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
-
+"""
 # Do not scale by std
 scaler = StandardScaler(with_std=False)
 scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-
-#X_train_scaled = X_train
-#X_test_scaled = X_test
+"""
+X_train_scaled = X_train
+X_test_scaled = X_test
 
 
 
@@ -58,7 +57,7 @@ lambdas = np.logspace(-4, 1, nlambdas)
 for i in range(nlambdas):
     lmb = lambdas[i]
     OwnRidgeBeta = np.linalg.pinv(X_train_scaled.T @ X_train_scaled+lmb*I) @ X_train_scaled.T @ y_train
-    RegRidge = linear_model.Ridge(lmb,fit_intercept=False)#True, normalize=False)
+    RegRidge = linear_model.Ridge(lmb,fit_intercept=False)#,normalize=True)
     RegRidge.fit(X_train_scaled,y_train)
     ypredictOwnRidge = X_test_scaled @ OwnRidgeBeta
     print("Values for own Ridge prediction")
