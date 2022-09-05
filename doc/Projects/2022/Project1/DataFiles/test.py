3a99e6d57e54aa7c23cc6735ea6cbb6138a7f663
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2022-09-05 21:56:01 +0200

    added files

---
+++ doc/Projects/2022/Project1/DataFiles/test.py
@@ -0,0 +1,30 @@
+import numpy as np
+from imageio import imread
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import numpy as np
+from numpy.random import normal, uniform
+
+# Load the terrain
+terrain = imread('SRTM_data_Norway_1.tif')
+
+N = 1000
+m = 5 # polynomial order
+terrain = terrain[:N,:N]
+# Creates mesh of image pixels
+x = np.linspace(0,1, np.shape(terrain)[0])
+y = np.linspace(0,1, np.shape(terrain)[1])
+x_mesh, y_mesh = np.meshgrid(x,y)
+
+z = terrain
+X = create_X(x_mesh, y_mesh,m)
+
+
+# Show the terrain
+plt.figure()
+plt.title('Terrain over Norway 1')
+plt.imshow(terrain, cmap='gray')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
