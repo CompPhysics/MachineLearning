585ea681bd03b496d95c32c6f18419a4329d92d5
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2022-10-04 14:12:48 +0200

    update

--- doc/src/week39/adagrad.py
+++ doc/src/week39/adagrad.py
@@ -4,53 +4,28 @@ from random import random, seed
 import numpy as np
 import autograd.numpy as np
 import matplotlib.pyplot as plt
-from autograd import grad
-
-# Note change from previous example
-def CostOLS(y,X,theta):
-    return np.sum((y-X @ theta)**2)
 
 n = 10000
 x = np.random.rand(n,1)
-y = 2.0+3*x +4*x*x# +np.random.randn(n,1)
-
+y = 4*x+3*x*x
+# Setting up Design matrix
 X = np.c_[np.ones((n,1)), x, x*x]
-XT_X = X.T @ X
-theta_linreg = np.linalg.pinv(XT_X) @ (X.T @ y)
+XTX = X.T @ X
+XTy = X.T @ y
+theta_linreg = np.linalg.pinv(XTX) @ (XTy)
 print("Own inversion")
 print(theta_linreg)
 
 
-# Note that we request the derivative wrt third argument (theta, 2 here)
-training_gradient = grad(CostOLS,2)
-# Define parameters for Stochastic Gradient Descent
-n_epochs = 50
-M = 5   #size of each minibatch
-m = int(n/M) #number of minibatches
-# Guess for unknown parameters theta
-theta = np.random.randn(3,1)
-gradients = np.zeros(theta.shape)
-r = gradients*gradients
-print(r.shape)
-print(gradients.shape)
-# Value for learning rate
+beta = np.random.randn(3,1)
 eta = 0.01
-# Including AdaGrad parameter to avoid possible division by zero
-delta  = 1e-8
-for epoch in range(n_epochs):
-    for i in range(m):
-        random_index = M*np.random.randint(m)
-        xi = X[random_index:random_index+M]
-        yi = y[random_index:random_index+M]
-        gradients = (1.0/M)*training_gradient(yi, xi, theta)
-        # calculate squared gradient by Hadamard multiplication
-#        r += (gradients*gradients)
-        r = np.sum(gradients*gradients)        
-        # compute update
-        update = 1.0/(delta+np.sqrt(r))*gradients
-        theta -= eta*update
-print("theta from own AdaGrad")
-print(theta)
+delta = 1e-8
+Niterations = 10000
+Giter = np.zeros(shape=(3,3))
+for iter in range(Niterations):
+    gradient = (2.0/n)*(XTX @ beta - XTy)
+    Giter +=gradient @ gradient.T
+    Ginverse = np.c_[eta/(delta+np.sqrt(np.diagonal(Giter)))]
+    beta -= np.multiply(Ginverse,gradient)
 
-
-
+print("Optimal parameters with AdaGrad",beta)
