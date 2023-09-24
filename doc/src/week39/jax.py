bff99c3750598cf248ce99c41fafa8cbe9f10c8c
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2023-09-24 22:04:27 +0200

    update

--- doc/src/week39/jax.py
+++ doc/src/week39/jax.py
@@ -1,9 +1,10 @@
-import jax.numpy as jnp
+import jax.nuympy as jnp
 from jax import grad, jit, vmap
+from jax import random
 
 def sum_logistic(x):
   return jnp.sum(1.0 / (1.0 + jnp.exp(-x)))
 
-x_small = jnp.arange(3.)
+x_small = arange(3.)
 derivative_fn = grad(sum_logistic)
 print(derivative_fn(x_small))
