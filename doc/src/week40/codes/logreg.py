f4421b6244543e5bc453514c6452b15ff4405eb2
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2024-09-30 06:28:37 +0200

    update on codes

--- doc/src/week40/codes/logreg.py
+++ doc/src/week40/codes/logreg.py
@@ -3,30 +3,29 @@ class LogisticRegression:
     def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
-        self.weights = None
+        self.beta_logreg = None
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
-    def fit(self, X, y):
-        num_samples, num_features = X.shape
-        self.weights = np.zeros(num_features)
+    def GDfit(self, X, y):
+        n_data, num_features = X.shape
+        self.beta_logreg = np.zeros(num_features)
         for _ in range(self.num_iterations):
-            linear_model = np.dot(X, self.weights)
+            linear_model = X @ self.beta_logreg
             y_predicted = self.sigmoid(linear_model)
             # Gradient calculation
-            gradient = np.dot(X.T, (y_predicted - y)) / num_samples
-            
-            # Update weights
-            self.weights -= self.learning_rate * gradient
+            gradient = (X.T @ (y_predicted - y))/n_data
+            # Update beta_logreg
+            self.beta_logreg -= self.learning_rate*gradient
     def predict(self, X):
-        linear_model = np.dot(X, self.weights)
+        linear_model = X @ self.beta_logreg
         y_predicted = self.sigmoid(linear_model)
         return [1 if i >= 0.5 else 0 for i in y_predicted]
 # Example usage
 if __name__ == "__main__":
     # Sample data
     X = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
-    y = np.array([0, 0, 0, 1])  # AND gate
-    model = LogisticRegression(learning_rate=0.1, num_iterations=1000)
-    model.fit(X, y)
+    y = np.array([0, 0, 0, 1])  # This is an AND gate
+    model = LogisticRegression(learning_rate=0.01, num_iterations=1000)
+    model.GDfit(X, y)
     predictions = model.predict(X)
     print("Predictions:", predictions)
--- doc/src/week40/codes/regression.py
+++ doc/src/week40/codes/regression.py
@@ -1,107 +1,43 @@
 # Importing various packages
 from random import random, seed
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import sys
 
-# the number of datapoints
+# the number of datapoints with a 2nd-order polynomial
 n = 100
 x = 2*np.random.rand(n,1)
-y = 4+3*x+np.random.randn(n,1)
-
-X = np.c_[np.ones((n,1)), x]
-# Hessian matrix
-H = (2.0/n)* X.T @ X
-# Get the eigenvalues
-EigValues, EigVectors = np.linalg.eig(H)
-print(f"Eigenvalues of Hessian Matrix:{EigValues}")
-
-beta_linreg = np.linalg.pinv(X.T @ X) @ X.T @ y
-print(beta_linreg)
-beta = np.random.randn(2,1)
-
-eta = 1.0/np.max(EigValues)
-Niterations = 1000
-
-for iter in range(Niterations):
-    gradient = (2.0/n)*X.T @ (X @ beta-y)
-    beta -= eta*gradient
-
-print(beta)
-xnew = np.array([[0],[2]])
-xbnew = np.c_[np.ones((2,1)), xnew]
-ypredict = xbnew.dot(beta)
-ypredict2 = xbnew.dot(beta_linreg)
-plt.plot(xnew, ypredict, "r-")
-plt.plot(xnew, ypredict2, "b-")
-plt.plot(x, y ,'ro')
-plt.axis([0,2.0,0, 15.0])
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.title(r'Gradient descent example')
-plt.show()
-
-
-
-X = np.c_[np.ones((n,1)), x]
-XT_X = X.T @ X
-
-#Ridge parameter lambda
-lmbda  = 0.001
-Id = n*lmbda* np.eye(XT_X.shape[0])
-
-# Hessian matrix
-H = (2.0/n)* XT_X+2*lmbda* np.eye(XT_X.shape[0])
-# Get the eigenvalues
-EigValues, EigVectors = np.linalg.eig(H)
-print(f"Eigenvalues of Hessian Matrix:{EigValues}")
-
-
-beta_linreg = np.linalg.pinv(XT_X+Id) @ X.T @ y
-print(beta_linreg)
-# Start plain gradient descent
-beta = np.random.randn(2,1)
-
-eta = 1.0/np.max(EigValues)
+y = 4+3*x+5*x*x
+# Design matrix
+X = np.c_[np.ones((n,1)), x, x*x]
+# Learning rate and number of iterations
+eta = 0.05
 Niterations = 100
 
+# OLS part
+beta_OLS = np.random.randn(3,1)
+gradient = np.zeros(3)
 for iter in range(Niterations):
-    gradients = 2.0/n*X.T @ (X @ (beta)-y)+2*lmbda*beta
-    beta -= eta*gradients
+    gradient = (2.0/n)*X.T @ (X @ beta_OLS-y)
+    beta_OLS -= eta*gradient
+print('Parameters for OLS using gradient descent')    
+print(beta_OLS)
 
-print(beta)
-ypredict = X @ beta
-ypredict2 = X @ beta_linreg
-plt.plot(x, ypredict, "r-")
-plt.plot(x, ypredict2, "b-")
-plt.plot(x, y ,'ro')
-plt.axis([0,2.0,0, 15.0])
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.title(r'Gradient descent example for Ridge')
-plt.show()
-
-
-# And now with Lasso
-# Start plain gradient descent
-beta_lasso = np.random.randn(2,1)
-
-eta = 0.01
-Niterations = 100
+#Ridge and Lasso parameter Lambda
+Lambda  = 0.01
+Id = n*Lambda* np.eye((X.T @ X).shape[0])
+# Gradient descent with  Ridge
+beta_Ridge = np.random.randn(3,1)
+gradient = np.zeros(3)
 for iter in range(Niterations):
-    gradients = 2.0/n*X.T @ (X @ (beta)-y)+2*lmbda*np.sign(beta)
-    beta_lasso -= eta*gradients
+    gradients = 2.0/n*X.T @ (X @ beta_Ridge-y)+2*Lambda*beta_Ridge
+    beta_Ridge -= eta*gradients
+print('Parameters for Ridge using gradient descent')    
+print(beta_Ridge)
 
-print('Gradient descent with Lasso:', beta_lasso)
-ypredict = X @ beta_lasso
-plt.plot(x, ypredict, "r-")
-plt.plot(x, y ,'ro')
-plt.axis([0,2.0,0, 15.0])
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.title(r'Gradient descent example for Lasso')
-plt.show()
-
+# Gradient descent with Lasso
+beta_Lasso = np.random.randn(3,1)
+gradient = np.zeros(3)
+for iter in range(Niterations):
+    gradients = 2.0/n*X.T @ (X @ beta_Lasso-y)+Lambda*np.sign(beta_Lasso)
+    beta_Lasso -= eta*gradients
+print('Parameters for Lasso using gradient descent')    
+print(beta_Lasso)
