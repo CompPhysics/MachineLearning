6a0465c0947d0c5682aafedb07f93b431c5875cf
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2024-10-07 06:46:31 +0200

    update

--- doc/src/week41/make.sh
+++ doc/src/week41/make.sh
@@ -44,19 +44,34 @@ system doconce split_html $html.html --method=space10
 # Bootstrap style
 html=${name}-bs
 system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
-system doconce split_html $html.html --method=split --pagination --nav_button=bottom
+#system doconce split_html $html.html --method=split --pagination --nav_button=bottom
 
 # IPython notebook
 system doconce format ipynb $name $opt
+
+
+# LaTeX Beamer slides
+beamertheme=red_plain
+system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
+system doconce ptex2tex $name envir=minted
+# Add special packages
+doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
+system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
+system pdflatex -shell-escape ${name}
+system pdflatex -shell-escape ${name}
+cp $name.pdf ${name}.pdf
+cp $name.tex ${name}.tex
 
 
 # Publish
 dest=../../pub
 if [ ! -d $dest/$name ]; then
 mkdir $dest/$name
+mkdir $dest/$name/pdf
 mkdir $dest/$name/html
 mkdir $dest/$name/ipynb
 fi
+cp ${name}*.pdf $dest/$name/pdf
 cp -r ${name}*.html ._${name}*.html reveal.js $dest/$name/html
 
 # Figures: cannot just copy link, need to physically copy the files
@@ -77,3 +92,11 @@ EOF
 tar czf ${ipynb_tarfile} README.txt
 fi
 cp ${ipynb_tarfile} $dest/$name/ipynb
+
+
+
+
+
+
+
+
